242d30dc91d2d35ac538d675d56be37e81bec48b
Helmi Effendi <helmiefendi64@gmail.com>
2023-09-02 23:18:39 +0700

    Add : Navbar, Routes, Transaction Pages (#1)
    * first commit : event management app (dev)
    
    * add : navbar, routes, page-transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
-import './App.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Transaction from './pages/user/Transaction';
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
+      <Route path="transaction" element={<Transaction />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <h1>HELLO WORLD!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

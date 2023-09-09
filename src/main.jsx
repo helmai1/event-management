9825228f60360872b71fa2a85ff37dd90a754eb5
raisrahayu <rais.rahayu@gmail.com>
2023-09-09 15:13:48 +0700

    register and login

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './redux/reducers';
 
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
